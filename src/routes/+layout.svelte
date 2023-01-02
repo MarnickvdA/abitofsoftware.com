2de60968d8f762f05070c15eaa63f4ad00b05de0
Marnick van der Arend <marnick@vdarend.com>
2023-01-02 17:33:20 +0100

    Fixed dark theme styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   export let data
 </script>
 
-<div class="mx-auto max-w-5xl min-h-screen bg-white px-8 border-x border-slate-900/15">
+<div class="mx-auto max-w-5xl min-h-screen bg-white dark:bg-slate-800 px-8 border-x border-slate-900/15 dark:border-slate-900">
     {#key data.currentRoute}
         <main class="pt-16" in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
             <slot />
